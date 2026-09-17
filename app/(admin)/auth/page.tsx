0614099bda5aaf0b2d5eb6cd7bd633bad83710ec
Celio junior <demon.junior13@gmail.com>
2026-09-17 11:20:35 +0000

    init files admin auth

---
+++ app/(admin)/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthPage(){
+    return <>
+    <div>
+        <h1>login Dashboard</h1>
+    </div>
+    </>
+}
---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,5 @@
+export default function RootLayoutAdmin({children}: LayoutProps<"/">){
+    return <>
+    {children}
+    </>
+}
